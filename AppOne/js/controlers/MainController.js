3e4a9d69f8de199f4ac0abe127e2ad8cef8747af
jake32321 <jake32321@yahoo.com>
2015-11-21 16:19:00 -0600

    added another scope to the index

--- AppOne/js/controlers/MainController.js
+++ AppOne/js/controlers/MainController.js
@@ -1,4 +1,5 @@
 //Purpose is to controll the app's data.
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'Top Sellers in Books'; //Properties are attached to the scope ($scope).
+  $scope.title = 'Check This Out'; //Properties are attached to the scope ($scope).
+  $scope.promo = 'What Is Up With Butter?';
 }]);
