46aceb49b73c0f14cf68d63cd9691d10ca19fd26
jake32321 <jake32321@yahoo.com>
2015-11-25 12:42:01 -0600

    Added new property to the scope with a list of attributes

--- AppOne/js/controlers/MainController.js
+++ AppOne/js/controlers/MainController.js
@@ -2,4 +2,8 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Check This Out'; //Properties are attached to the scope ($scope).
   $scope.promo = 'What Is Up With Butter?';
+  $scope.product = { //Different properties of a scope can have a list of attributes.
+    name: 'The Book of Trees',
+    price: 19
+  }
 }]);
